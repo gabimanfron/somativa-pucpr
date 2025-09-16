cd2b8c118455bf2f1306d74af5412d6996480cc5
gabimanfron <manfrongab@gmail.com>
2025-09-16 14:00:52 -0300

    pytest

---
+++ test/test.py
@@ -0,0 +1,44 @@
+from src.main import *;
+from unittest.mock import patch;
+
+def test_root():
+    result = root()
+    yield result
+    assert result == {"mensagem": "🚀 FastAPI rodando certinho!"}
+
+
+def test_funcaoteste():
+    with patch('randon.randint', return_value=12345):
+        result = funcaoteste()
+        yield result
+    
+    assert result == {"teste": True, "num_aleatorio": 12345}
+
+
+def test_create_estudante():
+    estudante_teste = Estudante(name='Carlos', curso='Matemática', ativo='False')
+    result = create_estudante(estudante_teste)
+    yield result
+    assert estudante_teste == result
+
+
+def test_update_estudante_negativo():
+    result = update_estudante(-5)
+    yield result 
+    assert not result
+
+def test_update_estudante_positivo():
+    result =  update_estudante(10)
+    yield result
+    assert result
+
+def test_delete_estudante_negativo():
+    result = delete_estudante(-5)
+    yield result
+    assert not result
+
+def test_delete_estudante_positivo():
+    result = delete_estudante(10)
+    yield result
+    assert not result
+
